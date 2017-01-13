c34a801277bb07f3c52afafb4e011829e3237f19
Joel <waylaidwanderer@live.com>
2017-01-13 09:47:38 -0800

    f

--- app/src/main/java/com/nkanaev/comics/managers/Utils.java
+++ app/src/main/java/com/nkanaev/comics/managers/Utils.java
@@ -7,21 +7,14 @@ import android.os.Build;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.util.DisplayMetrics;
-import android.util.Log;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 import java.security.MessageDigest;
 
 import static android.content.Context.ACTIVITY_SERVICE;
 
 
 public final class Utils {
-    private static final String TAG = Utils.class.getSimpleName();
-
     public static int getScreenDpWidth(Context context) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         return Math.round(displayMetrics.widthPixels / displayMetrics.density);
@@ -166,47 +159,5 @@ public final class Utils {
         } finally {
             output.close();
         }
-    }
-
-    public static String getUrl(String reqUrl) {
-        String response = null;
-        try {
-            URL url = new URL(reqUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            // read the response
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            response = convertStreamToString(in);
-        } catch (MalformedURLException e) {
-            Log.e(TAG, "MalformedURLException: " + e.getMessage());
-        } catch (ProtocolException e) {
-            Log.e(TAG, "ProtocolException: " + e.getMessage());
-        } catch (IOException e) {
-            Log.e(TAG, "IOException: " + e.getMessage());
-        } catch (Exception e) {
-            Log.e(TAG, "Exception: " + e.getMessage());
-        }
-        return response;
-    }
-
-    private static String convertStreamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line).append('\n');
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
     }
 }
